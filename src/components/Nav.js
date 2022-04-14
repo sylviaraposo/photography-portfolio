34816f1ef36e913662408d02f0f5f8e37a188b9c
sylvia <sylviaraposo@hotmail.ca>
2022-04-14 18:10:17 -0400

    added mobile styling

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -5,7 +5,7 @@ function Nav() {
     return (
         <div className="navBar">
             <nav>
-                <img className ="logo" src="./assets/logo.png" alt="picket-fence photography logo" />
+                <a href="#home"><img className="logo" src="./assets/logo.png" alt="picket-fence photography logo" /></a>
                 <ul>
                     <li><a href="">home</a></li>
                     <li><a href="">gallery</a></li>
