78563bc53021564a34d025fdf3cdbe141ea2e25c
sylvia <sylviaraposo@hotmail.ca>
2022-04-15 13:31:52 -0400

    mobile footer font size

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,9 +1,8 @@
 function Footer() {
     return (
         <footer>
-            <hr />
-            <p>made with love by <a href="www.sylviaraposo.com">Sylvia Raposo</a> ♥ 2022</p>
-            <hr />
+            <p className="footer-text">made with love by <a href="www.sylviaraposo.com">Sylvia Raposo</a> ♥ 2022</p>
+            
         </footer>
     )
 
--- src/images.js
+++ src/images.js
@@ -1,18 +1,18 @@
 export default [
     {
         src: './assets/1.jpg',
-        text: ' | Family | '
+        text: ' Located in Clarington, Ontario '
     },
     {
         src: './assets/2.jpg',
-        text: ' | Newborn and Toddlers | '
+        text: ' Capturing the special moments '
     },
     {
         src: './assets/3.jpg',
-        text: '| Lifestyle |'
+        text: ' Specializing in family, lifestyle and couple photography '
     }, 
     {
         src: './assets/4.jpg',
-        text: ' | Couples | '
+        text: ' 5+ year experienced photographer '
     }
 ]
