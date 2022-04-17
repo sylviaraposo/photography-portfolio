58a4e77aadd5162c0a3993e1aba02d59810fab62
sylvia <sylviaraposo@hotmail.ca>
2022-04-17 15:30:47 -0400

    updated footer text;

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,7 +2,7 @@ function Footer() {
     return (
         <footer>
             <hr />
-            <p className="footer-text">made with love by <a href="www.sylviaraposo.com">Sylvia Raposo</a> | 2022</p>
+            <p className="footer-text">made and designed with love by <a href="www.sylviaraposo.com">Sylvia Raposo</a> | 2022</p>
             
         </footer>
     )
